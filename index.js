3ad53adc29fd962a2238712ad33497e5ffd6a9b9
Alex <laingawbl@gmail.com>
2017-11-04 14:06:33 -0700

    implemented rotation and plane shifts

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-var app = require('express')();
-var io = require('socket.io')(8080);
+var app = require('express')()
+var session = require('express-session')
+var io = require('socket.io')(8081)
+
+app.listen(8080)
+
+drawing = [];
+
+drawing.push([{'x': -1, 'y': -1, 'z': -1},
+							{'x': -1, 'y': -1, 'z': 1},
+							{'x': 1, 'y': -1, 'z': 1},
+							{'x': 1, 'y': -1, 'z': -1},
+							{'x': -1, 'y': -1, 'z': -1}]);
+							
+app.use(session({
+	secret: 'draw',
+	resave: false,
+	saveUninitialized: true
+}))
 
 app.get('/', function(req, res){
-	res.sendfile(__dirname + '/draw.html');
-});
+	res.sendfile('./draw.html')
+})
 
-io.on('connection', function(sock){
-	sock.emit(
-});
+io.on('connection', function (socket) {
+	socket.emit('open', { 'serverIp': 	socket.request.headers.host})
+  socket.on('addition', function(data){	
+  	console.log(data)
+  	drawing.push(data);
+  	socket.emit('update', drawing);
+  })
+})
 
-app.listen(8080);
+
