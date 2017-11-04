69d77493889c1b40fe8c0eb85d5717d196ec9bc0
Alex <laingawbl@gmail.com>
2017-11-04 16:20:56 -0700

    final commit

--- index.js
+++ index.js
@@ -1,31 +1,34 @@
 var app = require('express')()
-var session = require('express-session')
 var io = require('socket.io')(8081)
 
 app.listen(8080)
 
 drawing = [];
-
 drawing.push([{'x': -1, 'y': -1, 'z': -1},
 							{'x': -1, 'y': -1, 'z': 1},
 							{'x': 1, 'y': -1, 'z': 1},
 							{'x': 1, 'y': -1, 'z': -1},
 							{'x': -1, 'y': -1, 'z': -1}]);
 							
-app.use(session({
-	secret: 'draw',
-	resave: false,
-	saveUninitialized: true
-}))
+drawing.push([{'x': -1, 'y': 1, 'z': -1},
+							{'x': -1, 'y': 1, 'z': 1},
+							{'x': 1, 'y': 1, 'z': 1},
+							{'x': 1, 'y': 1, 'z': -1},
+							{'x': -1, 'y': 1, 'z': -1}]);		
+							
+drawing.push([{'x': 1, 'y': 1, 'z': 1}, {'x': 1, 'y': -1, 'z': 1}]);
+drawing.push([{'x': 1, 'y': 1, 'z': -1}, {'x': 1, 'y': -1, 'z': -1}]);		
+drawing.push([{'x': -1, 'y': 1, 'z': -1}, {'x': -1, 'y': -1, 'z': -1}]);		
+drawing.push([{'x': -1, 'y': 1, 'z': 1}, {'x': -1, 'y': -1, 'z': 1}]);	
 
 app.get('/', function(req, res){
 	res.sendfile('./draw.html')
 })
 
 io.on('connection', function (socket) {
-	socket.emit('open', { 'serverIp': 	socket.request.headers.host})
+	console.log(socket.request.headers.origin);
+	socket.emit('open', {'serverIp': 	socket.request.headers.host})
   socket.on('addition', function(data){	
-  	console.log(data)
   	drawing.push(data);
   	socket.emit('update', drawing);
   })
